4fa07ec84e7cea3261f14464c63505a3f2d81f5f
Arjun <arjun.bali96@gmail.com>
2014-06-16 12:26:13 -0400

    passes all tests except last three

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,15 +1,20 @@
 def translate(string)
 	vowels = %w(a e i o u)
 	counter = 0
+	@output = []
 	to_be_added = ""
-	string.each_char do |x|
-		if vowels.include?(x)
-			break
-		else
-			to_be_added << x
-			counter += 1
+	words = string.split(' ')
+		words.each do |word|
+		word.each_char do |x|
+			if vowels.include?(x)
+				break
+			else
+				to_be_added << x
+				counter += 1
+			end
 		end
-	end
-		new_str = string + to_be_added.to_s + "ay"
-		new_str[counter..-1]
+		@new_str = word + to_be_added + "ay"
+		@output << @new_str[counter..-1]
+		end
+	@output.join(' ')
 end
