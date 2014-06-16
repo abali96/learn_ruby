7d1fe61baecf0561a8e6e4d6092bb5be24a6cf58
Arjun <arjun.bali96@gmail.com>
2014-06-16 11:29:32 -0400

    completed 10

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,19 +1,60 @@
 class Temperature
-	attr_accessor :unit, :temp
+	attr_accessor :f, :c
+#attrs means attributes
+	def initialize(attrs = Hash.new(0))
+		if attrs[:f]
+			@f = attrs[:f]
+			@c = Temperature.ftoc(attrs[:f])
+		elsif attrs[:c]
+			@c = attrs[:c]
+			@f = Temperature.ctof(attrs[:c])
+		end 
+	end
 
-	def initialize({:unit => num)
-		@temp = 
-		@unit = unit
+	def self.in_celsius(c)
+		self.new(:c => c)
+		#basically calling your initializer again
+	end
+
+	def self.in_fahrenheit(f)
+		self.new(:f => f)
 	end
 
 	def to_celsius
-		hash = Hash.new(@unit, @temp)
-		self.each do |key, value|
-			temp = (value-32)*(5.0/9)
-		end
+		@c.to_i
 	end
 
-	def to_farenheit
+	def to_fahrenheit
+		@f.to_i
+	end
+
+	def self.ftoc(f)
+		(f-32)*(5.0/9)
+	end
+
+	def self.ctof(c)
+		c*(9.0/5)+32
+	end
+
+end
+
+class Celsius < Temperature
+	def initialize(c)
+		@c = c
+	end
+
+	def to_fahrenheit
+		@c*(9.0/5)+32
+	end
+end
+
+class Fahrenheit < Temperature
+	def initialize(f)
+		@f =f
+	end
+
+	def to_celsius
+		(@f-32)*(5.0/9)
 	end
 
 end
